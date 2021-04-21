6bf5c32962a264aefa43711247ec75af827d4720
Ganesh Vernekar <ganeshvern@gmail.com>
2021-04-21 22:13:59 +0530

    AlertingNG: Add webhook notification channel
    Signed-off-by: Ganesh Vernekar <ganeshvern@gmail.com>

--- pkg/services/ngalert/notifier/channels/slack.go
+++ pkg/services/ngalert/notifier/channels/slack.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/url"
 	"regexp"
 	"strings"
 	"time"
@@ -28,9 +27,8 @@ import (
 // alert notification to Slack.
 type SlackNotifier struct {
 	old_notifiers.NotifierBase
-	log         log.Logger
-	tmpl        *template.Template
-	externalUrl *url.URL
+	log  log.Logger
+	tmpl *template.Template
 
 	URL            string
 	Username       string
@@ -49,7 +47,7 @@ type SlackNotifier struct {
 var reRecipient *regexp.Regexp = regexp.MustCompile("^((@[a-z0-9][a-zA-Z0-9._-]*)|(#[^ .A-Z]{1,79})|([a-zA-Z0-9]+))$")
 
 // NewSlackNotifier is the constructor for the Slack notifier
-func NewSlackNotifier(model *models.AlertNotification, t *template.Template, externalUrl *url.URL) (*SlackNotifier, error) {
+func NewSlackNotifier(model *models.AlertNotification, t *template.Template) (*SlackNotifier, error) {
 	if model.Settings == nil {
 		return nil, alerting.ValidationError{Reason: "No Settings Supplied"}
 	}
@@ -105,7 +103,6 @@ func NewSlackNotifier(model *models.AlertNotification, t *template.Template, ext
 		Fallback:       model.Settings.Get("fallback").MustString(`{{ template "slack.default.title" . }}`),
 		log:            log.New("alerting.notifier.slack"),
 		tmpl:           t,
-		externalUrl:    externalUrl,
 	}, nil
 }
 
@@ -166,7 +163,7 @@ func (sn *SlackNotifier) Notify(ctx context.Context, as ...*types.Alert) (bool, 
 
 func (sn *SlackNotifier) buildSlackMessage(ctx context.Context, as []*types.Alert) (*slackMessage, error) {
 	var tmplErr error
-	data := notify.GetTemplateData(ctx, &template.Template{ExternalURL: sn.externalUrl}, as, gokit_log.NewNopLogger())
+	data := notify.GetTemplateData(ctx, sn.tmpl, as, gokit_log.NewNopLogger())
 	alerts := types.Alerts(as...)
 	tmpl := notify.TmplText(sn.tmpl, data, &tmplErr)
 
